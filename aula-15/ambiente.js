dc89648f3a592a34f419f994356e104e90dc9356
Fabio Junio <83479976+Fabiojunio12@users.noreply.github.com>
2025-03-05 15:56:48 -0300

    conteúdo da aula 15

---
+++ aula-15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [3, 5, 10, 6, 8];
+num.sort((a, b) => a - b);
+num.push(1);
+
+console.log(num);
+console.log(`o vetor tem ${num.length} posições.`);
+console.log(`O primeiro valor do vetor é ${num[0]}`);
+
+let pos = num.indexOf(10);
+if (pos == -1) {
+    console.log('Valor não foi encontrado!');
+} else {
+    console.log(`O valor está na posição ${pos}`);
+}
