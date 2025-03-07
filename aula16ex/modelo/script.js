687711f118b90f88a14a60bdbd1b42189c7e75c5
Fabio Junio <83479976+Fabiojunio12@users.noreply.github.com>
2025-03-07 20:32:40 -0300

    exercicio resolvido pelo prof guanabara

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -1,45 +1,65 @@
-let arraynum = [];
-let maior = 0;
-let menor = 100;
-let soma = 0;
-let media = 0;
-let res = document.getElementById('res');
+let num = document.querySelector('input#fnum');
+let lista = document.querySelector('select#flista');
+let res = document.querySelector('div#res');
+let valores = [];
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
+    } else {
+        return false;
+    };
+};
 
 function adicionar() {
-    let num = document.getElementById('numeros');
-    let ressel = document.getElementById('ressel');
-
-    let n = Number(num.value);
-
-    if (n < 1 | n == "" | n > 100 | arraynum.indexOf(n) != -1) {
-        alert('[ERRO!] Número inválido ou já adicionado. Digite um números entre 1 e 100');
-    } else {
-        res.innerHTML = '';
-        soma += n;
-        arraynum.push(n);
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value));
         let item = document.createElement('option');
-        item.text = `Número ${n} foi adicionado`;
-        ressel.appendChild(item);
-        if (maior < n) {
-            maior = n;
-        }
-        if (menor > n) {
-            menor = n;
-        }
-    }
-}
+        item.text = `Valor ${num.value} adicionado.`;
+        lista.appendChild(item);
+        res.innerHTML = '';
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')
+    };
+    num.value = '';
+    num.focus();
+};
 
 function finalizar() {
-    if (arraynum != '') {
-        media = soma / arraynum.length;
-        res.innerHTML += `
-            <p>Você digitou ${arraynum.length} números.</p>
-            <p>O maior número foi ${maior}</p>
-            <p>O menor número foi ${menor}</p>
-            <p>A somar entre eles é ${soma}</p>
-            <p>A média dos valores é ${media}</p>
-        `;
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar.');
     } else {
-        alert('[ERROR!] sem valores adicionados')
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        let media = 0;
+
+        for (pos in valores) {
+            soma += valores[pos];
+            if (valores[pos] > maior) {
+                maior = valores[pos];
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos];
+            }
+        }
+
+        media = soma / tot;
+
+        res.innerHTML = '';
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+        res.innerHTML += `<p>O maior número informado foi ${maior}.</p>`;
+        res.innerHTML += `<p>O menor número informado foi ${menor}.</p>`;
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`;
+        res.innerHTML += `A média dos valores digitados é ${media}.`;
     }
-}
+};
