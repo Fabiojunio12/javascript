a5b2111fd0f766a7f723bd44bf0eeedd832cfabe
Fabio Junio <83479976+Fabiojunio12@users.noreply.github.com>
2025-09-13 16:53:50 -0300

    adição de vários exercicios

---
+++ descomplica/DesenvolvimentoDinamico/funcoes/atividade-pratica-01.js
@@ -0,0 +1,21 @@
+function saudacao() {
+    console.log("Olá, mundo!");
+};
+
+function novaSaudacao(nome) {
+    console.log(`Olá ${nome}, seja bem vindo!`);
+};
+
+function soma(a, b) {
+    return a + b;
+};
+
+saudacao();
+
+novaSaudacao("Fábio Junio");
+novaSaudacao("Mikaele");
+novaSaudacao("Teresa");
+
+console.log(soma(25, 23));
+console.log(soma(12, 13));
+console.log(soma(52, 5));
---
+++ exerciciosChatGpt/desafio001.js
@@ -0,0 +1,15 @@
+// Entrada de dados
+let numero1 = 500;
+let numero2 = 300;
+
+// Processamento de dados
+let resultadoSoma = numero1 + numero2;
+let resultadoSubtracao = numero1 - numero2;
+let resultadoMultiplicacao = numero1 * numero2;
+let resultadoDivisao = numero1 / numero2;
+
+// Saída de dados
+console.log(`A soma entre ${numero1} + ${numero2} é igual: ${resultadoSoma}.`);
+console.log(`A subtração entre ${numero1} - ${numero2} é igual: ${resultadoSubtracao}.`);
+console.log(`A multiplicação entre ${numero1} * ${numero2} é igual: ${resultadoMultiplicacao}.`);
+console.log(`A divisão entre ${numero1} / ${numero2} é igual: ${resultadoDivisao}.`);
